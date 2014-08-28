9fca78758b987f9c491e503afabd2f0dfa0b7405
Lellansin <lellansin@gmail.com>
2014-08-28 16:04:58 +0800

    注释

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 module.exports = require('./lib/pomelo');
+
+/*
+  require 当前目录会变成 require('./lib/pomelo/index.js')
+  而 ./lib/pomelo/index.js 中又继续到处
+  最后 require 当前目录会变成 require('./lib/pomelo/pomelo.js')
+*/
